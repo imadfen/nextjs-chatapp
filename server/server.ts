963b48294acb9f927196930bed230e1484d37ec7
imadfen <imadfen20@gmail.com>
2023-06-18 00:12:59 +0100

    reset database daily & get back .env

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,22 @@
 import { Socket } from "socket.io";
 import { Server } from "socket.io";
+const cron = require('node-cron');
+import prisma from "../utils/prisma"
 
 console.log("socket starts on port 5000");
+
+// Scheduling the server to reset the database:
+cron.schedule('0 0 * * *', async () => {
+    try {
+        await prisma.userRoom.deleteMany()
+        await prisma.message.deleteMany()
+        await prisma.room.deleteMany()
+        await prisma.user.deleteMany()
+    } catch (error) {
+        console.error(error)
+    }
+});
+
 
 const io = new Server(5000, {
     cors: {
@@ -15,7 +30,7 @@ io.on('connection', (socket: Socket) => {
         socket.to(room).emit("receive-messages", room, message)
     })
 
-    
+
 
     socket.on("join-room", (room: string) => {
         socket.join(room)
