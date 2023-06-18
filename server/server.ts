19128ac021463499ba30f59713a844836a5f0f10
imadfen <imadfen20@gmail.com>
2023-06-18 16:22:48 +0100

    resetDB function

--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,13 @@
 import { Socket } from "socket.io";
 import { Server } from "socket.io";
 const cron = require('node-cron');
-import prisma from "../utils/prisma"
+import { resetDB } from "../utils/prisma"
 
 console.log("socket starts on port 5000");
 
 // Scheduling the server to reset the database:
 cron.schedule('0 0 * * *', async () => {
-    try {
-        await prisma.userRoom.deleteMany()
-        await prisma.message.deleteMany()
-        await prisma.room.deleteMany()
-        await prisma.user.deleteMany()
-    } catch (error) {
-        console.error(error)
-    }
+    await resetDB()
 });
 
 
@@ -29,7 +22,6 @@ io.on('connection', (socket: Socket) => {
     socket.on("send-message", (room, message) => {
         socket.to(room).emit("receive-messages", room, message)
     })
-
 
 
     socket.on("join-room", (room: string) => {
--- utils/prisma.ts
+++ utils/prisma.ts
@@ -2,4 +2,15 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+export async function resetDB() {
+    try {
+        await prisma.userRoom.deleteMany()
+        await prisma.message.deleteMany()
+        await prisma.room.deleteMany()
+        await prisma.user.deleteMany()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 export default prisma
